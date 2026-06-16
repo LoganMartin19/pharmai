7a6bbed8df8611fbe1ca5e626440fd08ff6050c2
Logan <Logan@stride.website>
2026-06-16 21:07:38 +0100

    Fix EAS iOS project configuration

---
+++ app.config.js
@@ -0,0 +1,24 @@
+const appJson = require('./app.json');
+
+module.exports = {
+  name: appJson.displayName,
+  slug: 'pharmai',
+  version: '1.0.0',
+  orientation: 'portrait',
+  scheme: 'pharmai',
+  ios: {
+    bundleIdentifier: 'Logan.PharmAI',
+    buildNumber: '2',
+    supportsTablet: false,
+    infoPlist: {
+      NSCameraUsageDescription: 'PharmAI uses the camera to scan prescriptions and medication bottles.',
+      NSLocationWhenInUseUsageDescription: 'PharmAI uses your location to show nearby pharmacies for refills.',
+      UIBackgroundModes: ['remote-notification'],
+    },
+  },
+  extra: {
+    eas: {
+      projectId: '1c93a68c-0a1a-48b9-bce4-e89e1fc7a7b3',
+    },
+  },
+};
